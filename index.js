b2965b8a3cfaab0ca33770d42c512a8d75ba0ab6
Max <80323354+GuyBritish@users.noreply.github.com>
2021-11-22 02:29:05 -0600

    Add starter code and packages

---
+++ index.js
@@ -0,0 +1,43 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+//=================================================================================================
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("ejs", ejsMate);
+
+//=================================================================================================
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500, message = "Something went wrong!" } = err;
+	if (!err.statusCode) {
+		err.statusCode = 500;
+	}
+	if (!err.message) {
+		err.message = "Something went wrong!";
+	}
+});
+
+//=================================================================================================
+
+const Port = process.env.PORT || 3000;
+
+app.listen(Port, () => {
+	console.log(`TicketViewer is listening on port ${Port}`);
+});
