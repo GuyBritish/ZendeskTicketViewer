be13b176676d4e52af6609f2c841b0220612b6b6
Max <80323354+GuyBritish@users.noreply.github.com>
2021-11-22 03:28:57 -0600

    Add get all tickets request

--- controllers/tickets_Control.js
+++ controllers/tickets_Control.js
@@ -1,6 +1,24 @@
+const axios = require("axios");
+
+const domain = process.env.DOMAIN;
+const user = process.env.EMAIL;
+const password = process.env.PASS;
+
+//=================================================================================================
+
 const indexPage = async (req, res) => {
-	const tickets = [1, 2, 3];
-	res.render("tickets/index", { tickets });
+	const options = {
+		url: `https://${domain}.zendesk.com/api/v2/tickets.json`,
+		method: "GET",
+		auth: {
+			username: `${user}`,
+			password: `${password}`,
+		},
+	};
+
+	const resp = await axios(options);
+	const ticketList = resp.tickets;
+	res.render("tickets/index", { ticketList });
 };
 
 //=================================================================================================
