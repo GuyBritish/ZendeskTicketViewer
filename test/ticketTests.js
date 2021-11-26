65d685d8c0190998c5725bad6767423162094ede
Max <80323354+GuyBritish@users.noreply.github.com>
2021-11-25 18:32:23 -0600

    Define test cases for ticket helper methods

--- test/ticketTests.js
+++ test/ticketTests.js
@@ -20,3 +20,28 @@ describe("Ticket Requests", () => {
 		it("Valid request for one ticket", () => {});
 	});
 });
+
+// Test the ticket helper methods for /tickets route's controller.
+describe("Tickets Helpers", () => {
+	// Test the getPageNumber() method if it functions correctly and throws appropriate errors.
+	describe("Get page number", () => {
+		it("Invalid page string (Not a number)", () => {});
+		it("Invalid page string (Empty)", () => {});
+		it("Valid page string", () => {});
+	});
+
+	// Test the getPageRange() method if it functions correctly.
+	describe("Get page range", () => {
+		it("Page number is the first/last of available pages", () => {});
+		it("Page range is larger than available pages", () => {});
+		it("Page range is smaller than available pages", () => {});
+	});
+
+	// Test the getTicketsForPage() method if it functions correctly and throws appropriate errors.
+	describe("Get the tickets for one page", () => {
+		it("Invalid page number", () => {});
+		it("The last page", () => {});
+		it("Not the last page", () => {});
+		it("No available tickets", () => {});
+	});
+});
