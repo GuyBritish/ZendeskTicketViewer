a2e582dc4560bf42c4f180577b0fe140781c1a17
Max <80323354+GuyBritish@users.noreply.github.com>
2021-11-26 04:21:01 -0600

    Remove tickets page slicing method

--- controllers/tickets_Helper.js
+++ controllers/tickets_Helper.js
@@ -4,25 +4,14 @@ const getPageNumber = (page) => {
 	return parseInt(page);
 };
 
-const getPageRange = (currPage, pages, range) => {
+const getPageRange = (currPage, count, perPage, range) => {
+	const pages = Math.ceil(count / perPage);
 	range = Math.abs(range);
 	let maxPage = Math.min(currPage + range, Math.max(currPage, pages));
 	let minPage = Math.max(currPage - range, Math.min(currPage, 1));
 	return { minPage, maxPage };
 };
 
-const getTicketsForPage = (ticketList, currPage, perPage) => {
-	const pages = Math.ceil(ticketList.length / perPage);
-
-	if (pages < currPage && currPage !== 1) throw new Error("Invalid page number!");
-
-	const tickets = ticketList.slice(
-		perPage * (currPage - 1),
-		Math.min(ticketList.length, perPage * currPage)
-	);
-	return { tickets, pages, currPage };
-};
-
 //=================================================================================================
 
-module.exports = { getPageNumber, getPageRange, getTicketsForPage };
+module.exports = { getPageNumber, getPageRange };
