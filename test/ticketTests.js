09a369048738d42b1cdcfaeef686080353ba51df
Max <80323354+GuyBritish@users.noreply.github.com>
2021-11-25 18:27:53 -0600

    Define test cases for ticket request methods

--- test/ticketTests.js
+++ test/ticketTests.js
@@ -1,3 +1,22 @@
 const chai = require("chai");
 const chatHttp = require("chai-http");
 const should = chai.should();
+
+// Test the ticket request methods to Zendesk API.
+describe("Ticket Requests", () => {
+	// Test the getAllTickets() method if it functions correctly and throws appropriate errors.
+	describe("GET all tickets", () => {
+		it("Incorrect request subdomain", () => {});
+		it("Incorrect user email", () => {});
+		it("Incorrect user password", () => {});
+		it("Valid request for all tickets", () => {});
+	});
+
+	// Test the getIndividualTicket() method if it functions correctly and throws appropriate errors.
+	describe("GET individual tickets", () => {
+		it("Incorrect request subdomain", () => {});
+		it("Incorrect user email", () => {});
+		it("Incorrect user password", () => {});
+		it("Valid request for one ticket", () => {});
+	});
+});
